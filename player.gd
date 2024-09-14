fdbc4d5351585648346bb00c0f65e4eea7c1d029
Cole Horvat <ColeHorvat@gmail.com>
2024-09-14 14:27:20 -0700

    Remove space to start

--- player.gd
+++ player.gd
@@ -2,13 +2,5 @@ extends PathFollow3D
 
 @export var move_speed: float = 4.0;
 
-var moving: bool = false;
-
-func _process(delta: float) -> void:
-	if (Input.is_action_just_pressed('ui_accept')):
-		moving = true;
-
-
 func _physics_process(delta: float) -> void:
-	if (moving):
-		progress += move_speed * delta;
+	progress += move_speed * delta;
